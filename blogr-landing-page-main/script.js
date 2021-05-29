896f78782a75ab0ff86e9582db194cc482d506b6
Adedeji Adetola <adetolaadedeji0@gmail.com>
2021-05-29 05:56:40 +0100

    destop version completed

--- blogr-landing-page-main/script.js
+++ blogr-landing-page-main/script.js
@@ -1,14 +1,81 @@
 
-//on click company, product, connect : sub -links should be displayed ---- on click main-li, display js-menu
+//document.click.body{ if one clicked..... else.....}
 
-/*
-onclick 'one', display:block
-onclick 'two', display:block and add js-menu, remove js-menu from others 
-onclick 'three' display:block add js-menu, remove from others*/
 
-document.querySelector('.one').addEventListener('click',function(){
-  document.querySelector('.product').classList.add('js-menu');
+document.querySelector('body').addEventListener('click', function(event){
+  if (event.target === document.querySelector('.one')) {
+    //On click product
+    
+      document.querySelector('.product').classList.toggle('js-menu');
+      document.querySelector('.company').classList.remove('js-menu');
+      document.querySelector('.connect').classList.remove('js-menu');
+  
+      document.querySelector('.fas0').classList.toggle('fa-chevron-up');
+      document.querySelector('.fas0').classList.toggle('fa-chevron-down');
+  
+      document.querySelector('.fas1').classList.remove('fa-chevron-up');
+      document.querySelector('.fas1').classList.add('fa-chevron-down');
+  
+      document.querySelector('.fas2').classList.remove('fa-chevron-up');
+      document.querySelector('.fas2').classList.add('fa-chevron-down');
+    
+  } else if (event.target === document.querySelector('.two')) {
+    
+    //On click company
+    
+      document.querySelector('.company').classList.toggle('js-menu');
+      document.querySelector('.connect').classList.remove('js-menu');
+      document.querySelector('.product').classList.remove('js-menu');
+  
+      document.querySelector('.fas1').classList.toggle('fa-chevron-up');
+      document.querySelector('.fas1').classList.toggle('fa-chevron-down');
+  
+      document.querySelector('.fas0').classList.remove('fa-chevron-up');
+      document.querySelector('.fas0').classList.add('fa-chevron-down');
+  
+      document.querySelector('.fas2').classList.remove('fa-chevron-up');
+      document.querySelector('.fas2').classList.add('fa-chevron-down');
+
+  } else if (event.target === document.querySelector('.three')) {
+    //On click connect
+    
+      document.querySelector('.connect').classList.toggle('js-menu');
+      document.querySelector('.product').classList.remove('js-menu');
+      document.querySelector('.company').classList.remove('js-menu');
+  
+      document.querySelector('.fas2').classList.toggle('fa-chevron-up');
+      document.querySelector('.fas2').classList.toggle('fa-chevron-down');
+  
+      document.querySelector('.fas0').classList.remove('fa-chevron-up');
+      document.querySelector('.fas0').classList.add('fa-chevron-down');
+  
+      document.querySelector('.fas1').classList.remove('fa-chevron-up');
+      document.querySelector('.fas1').classList.add('fa-chevron-down');
+
+  } else {
+    document.querySelector('.fas0').classList.remove('fa-chevron-up');
+    document.querySelector('.fas0').classList.add('fa-chevron-down');
+  
+    document.querySelector('.fas1').classList.remove('fa-chevron-up');
+    document.querySelector('.fas1').classList.add('fa-chevron-down');
+  
+    document.querySelector('.fas2').classList.remove('fa-chevron-up');
+    document.querySelector('.fas2').classList.add('fa-chevron-down');
+  
+    document.querySelector('.connect').classList.remove('js-menu');
+    document.querySelector('.product').classList.remove('js-menu');
+    document.querySelector('.company').classList.remove('js-menu');
+  }
 })
 
-//adding and removing class list .js-menu
-// else onclick anywhere else display returns to none
+
+
+
+
+
+
+
+
+
+
+
